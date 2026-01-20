1a960a5ef5f3ee19c5e1e245ff667d4a9ad84a75
Noguchi Neo <enderneo0223@gmail.com>
2026-01-20 23:02:51 +0900

    Add HelloController with sample GET API

---
+++ PortfolioApi/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                message = "Hello Portfolio API!",
+                createdAt = DateTime.Now
+            });
+        }
+    }
+}
